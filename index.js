bd993bca1730e0f55bef048b849e37fe9db3af61
Jake <jakemoccia@gmail.com>
2024-10-11 11:32:39 -0400

    fixed api call and css

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 async function fetchPokemon() {
-    const pokemon = await fetch("https://pokeapi.co/api/v2/pokemon?limit=1302");
-    const pokemonData = await pokemon.json();
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=999");
+    const pokemonData = await response.json();
     console.log(pokemonData)
-    // const pokemonListEl = document.querySelector('.pokemon-list');
-    // pokemonData.map(poke =>
-    //  `<div class="pokemon__card">
-    //     <div class="pokemon__card--container">
-    //     <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${pokeData.id}.png" class="pokemon__img" alt="">
-    //     <h2>${pokeData.name}</h2>
-    //     <p>${pokeData.id}</p>
-    //     <p>Data Types</p>
-    //     </div>
-    // </div>`
-    // )
-    // .join("");
+    const pokemonListEl = document.querySelector(".pokemon__list");
+    pokemonListEl.innerHTML = pokemonData.results.map((poke, index) => {
+        const pokemonId = index + 1;
+        return `<div class="pokemon__card")}>
+        <div class="pokemon__card--container">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png" class="pokemon__img" alt="${poke.name}">
+        <h2>${capitalize(poke.name)}</h2>
+        <p>Pokedex ID: #${pokemonId}</p>
+        </div>
+        </div>`;
+    })
+    .join("");
+}
+
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function firstSixElements(str) {
+    return str.slice(0, 6);
 }
 
 fetchPokemon();
